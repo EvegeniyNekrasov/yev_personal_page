29aba2acf81dd76cbc124b044a6f257a0b19e85a
EvegeniyNekrasov <eanekrasovart@gmail.com>
2024-06-23 13:02:22 +0200

    feat: creating Navbar component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,44 +1,14 @@
 <script>
-	import { flip } from 'svelte/animate';
-	import { quintOut } from 'svelte/easing';
-
 	import '../app.css';
-
-	let links = [
-		{ name: 'Inicio', path: '/' },
-		{ name: 'Sobre mi', path: '/about' },
-		{ name: 'Trabajos', path: '/works' }
-	];
-
-	let activeLink = 'Inicio';
+	import Navbar from '../lib/components/Navbar/Navbar.svelte';
 
 	let date = new Date();
 	let year = date.getFullYear();
-
-	function setActive(link) {
-		activeLink = link;
-	}
 </script>
 
 <div class="wrapper">
 	<header>
-		<nav>
-			<ul>
-				{#each links as link (link.name)}
-					<li animate:flip={{ delay: 1000, duration: 1000, easing: quintOut }}>
-						<a
-							class:active={activeLink === link.name}
-							href={link.path}
-							on:click={(e) => {
-								setActive(link.name);
-							}}
-						>
-							{link.name}
-						</a>
-					</li>
-				{/each}
-			</ul>
-		</nav>
+		<Navbar />
 	</header>
 	<main>
 		<slot></slot>
@@ -73,15 +43,6 @@
 		padding: 10px 0;
 	}
 
-	header nav {
-		max-width: 1000px;
-		position: relative;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		margin: 20px auto 0;
-	}
-
 	main {
 		grid-area: main;
 		margin-top: 100px;
@@ -93,49 +54,5 @@
 		grid-area: footer;
 		text-align: center;
 		padding: 20px;
-	}
-
-	ul {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		gap: 20px;
-		list-style: none;
-	}
-
-	a {
-		text-decoration: none;
-		padding: 10px 15px;
-		display: inline-block;
-		color: var(--gray-12);
-		transition:
-			color 0.3s,
-			background-color 0.3s,
-			transform 0.3s;
-		animation: slideIn 0.5s ease-out;
-	}
-
-	a:hover {
-		color: var(--violet-10);
-		background-color: var(--violet-3);
-	}
-
-	a.active {
-		animation: slideIn 0.5s ease-out;
-		background-color: var(--violet-6);
-		border-radius: 4px;
-		color: var(--violet-10);
-		transform: scale(1.05);
-	}
-
-	@keyframes slideIn {
-		from {
-			opacity: 0;
-			transform: translateX(-10px);
-		}
-		to {
-			opacity: 1;
-			transform: translateX(0);
-		}
 	}
 </style>
